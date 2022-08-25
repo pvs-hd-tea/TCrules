e8116433b12c20f5bcfd1922280d439e88d7a4a4
Jonas Ochs <64967687+Jonas239@users.noreply.github.com>
2022-08-25 23:54:17 +0200

    concepts finalized
    doc needs to be updated, will do

--- c2c-translator/concepts.py
+++ c2c-translator/concepts.py
@@ -2,6 +2,7 @@ import os
 import json
 from tree_sitter import Language, Parser
 from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 Language.build_library(
     # Store the library in the `build` directory
@@ -45,6 +46,14 @@ class Concept:
                 self.concepts = json.load(file)
                 print(f"Loading {len(self.concepts)} concepts ... Done ...")
 
+        if not os.path.isfile("keywords_concept.txt"):
+            self.tree_concepts = []
+        else:
+            with open("keywords_concept.txt", encoding="utf8") as file:
+                self.tree_concepts = file.read().split(",")
+                print(
+                    f"Loading {len(self.tree_concepts)} keywords for concepts ... Done ...")
+
     def save_concepts(self):
         """store derived rules in json format"""
         with open("concepts.json", "a+", encoding="utf8") as file:
@@ -52,6 +61,17 @@ class Concept:
             file.seek(0)
             json.dump(self.concepts, file, indent=4)
 
+    def save_keywords(self):
+        """store keywords from own json"""
+        with open("keywords_concept.txt", "a+", encoding="utf8") as file:
+            file.truncate(0)
+            file.seek(0)
+            length = len(self.tree_concepts) - 1
+            for i, keyword in enumerate(self.tree_concepts):
+                if i == length:
+                    file.write(keyword)
+                else:
+                    file.write(keyword + ",")
 
     def extend_concept(self, cpp, java, python, key):
         """extend existing concept '<key>' by list of generic expressions, one for each language"""
@@ -68,7 +88,8 @@ class Concept:
                     break  # already present
 
             if not flag:
-                self.concepts[key].append([generic_cpp, generic_jv, generic_py])
+                self.concepts[key].append(
+                    [generic_cpp, generic_jv, generic_py])
 
     def add_concept(self, cpp, java, python, key):
         """add new concept to the database"""
@@ -77,13 +98,14 @@ class Concept:
         generic_py = self.create_generic_expression(python, PYTHON.lower())
 
         if generic_cpp and generic_jv and generic_py:
-            self.concepts.update({key: [[generic_cpp, generic_jv, generic_py]]})
+            self.concepts.update(
+                {key: [[generic_cpp, generic_jv, generic_py]]})
 
     def user_input(self, keyword=None):
         """Check for well known concept"""
         print("Please enter the name of the concept below. Try not to enter a concept twice and check for existing names first")
         print("Does one of the entries describe your code's concept?")
-        with open ("concepts.json") as concepts:
+        with open("concepts.json") as concepts:
             keys = list(concepts.keys())
         print(keys)
         print("If it doesn't resemble any concept, please enter a new one. Please try to consider newlines and indents for better accuracy in translations")
@@ -96,55 +118,86 @@ class Concept:
         else:
             self.add_concept(cpp, java, python, keyword)
 
-    def check_similarity(self,input_code, input_language):
-        parse_tree = create_parse_tree(input_code,input_language)
-        with open("concepts.json") as concepts:
-            values = list(concepts.values())
-        java_trees = []
-        cpp_trees = []
-        python_trees = []
+    def check_similarity(self, input_code, input_language):
+
+        parse_tree = create_parse_tree(input_code, input_language)
+        if input_language == "PYTHON":
+            lang_id = "py"
+        if input_language == "JAVA":
+            lang_id = "jv"
+        if input_language == "CPP":
+            lang_id = "cpp"
 
         if input_language == JAVA:
-            for value in values:
-                tree = parser_jv.parse(bytes(value, "utf-8")).root_node.sexp()
-                ratio = fuzz.partial_ratio(parse_tree,tree)
+            lang_matches = []
+            for concept in self.tree_concepts:
+                if lang_id in concept:
+                    tree = parser_jv.parse(
+                        bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
+                    lang_matches.append(tree)
+                    final_concept = process.extractOne(
+                        parse_tree, lang_matches, scorer=fuzz.partial_ratio)
+                    if final_concept[-1] >= 80:
+                        concept_final = concept
         if input_language == CPP:
-            for value in values:
-                tree = parser_jv.parse(bytes(value, "utf-8")).root_node.sexp()
-                ratio = fuzz.partial_ratio(parse_tree,tree)
+            lang_matches = []
+            for concept in self.tree_concepts:
+                if lang_id in concept:
+                    tree = parser_cpp.parse(
+                        bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
+                    lang_matches.append(tree)
+                    final_concept = process.extractOne(
+                        parse_tree, lang_matches, scorer=fuzz.partial_ratio)
+                    if final_concept[-1] >= 80:
+                        concept_final = concept
         if input_language == PYTHON:
-            for value in values:
-                tree = parser_jv.parse(bytes(value, "utf-8")).root_node.sexp()
-                ratio = fuzz.partial_ratio(parse_tree,tree)
+            lang_matches = []
+            for concept in self.tree_concepts:
+                if lang_id in concept:
+                    tree = parser_py.parse(
+                        bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
+                    lang_matches.append(tree)
+                    final_concept = process.extractOne(
+                        parse_tree, lang_matches, scorer=fuzz.partial_ratio)
+                    if final_concept[-1] >= 80:
+                        concept_final = concept
+        print(concept_final + "has thet highest posssibility of being a correct match. Suggestion for optimized code is in suggestion.txt")
+        with open('suggestion.txt', 'w') as datas:
+            concept_string = concept_final[:-3]
+            concept_string_py = concept_string + "_py"
+            concept_string_jv = concept_string + "_jv"
+            concept_string_cpp = concept_string + "_cpp"
+
+            datas.write("PYTHON:\n")
+            datas.write(str(self.concepts[concept_string_py][0][0]))
+            datas.write("\n")
+            datas.write("---------------------------------------------\n")
+            datas.write("JAVA:\n")
+            datas.write(str(self.concepts[concept_string_jv][0][0]))
+            datas.write("\n")
+            datas.write("---------------------------------------------\n")
+            datas.write("CPP:\n")
+            datas.write(str(self.concepts[concept_string_cpp][0][0]))
+
+
+
         
-            
 
 
 def create_parse_tree(input_code, input_language):
     """return s-expression and parse tree for the given code and language using the tree-sitter"""
     if input_language == "CPP":
-        return parser_cpp.parse(bytes(input_code, "utf-8")).root_node.sexp(), parser_cpp.parse(bytes(input_code, "utf-8"))
+        return parser_cpp.parse(bytes(input_code, "utf-8")).root_node.sexp()
     if input_language == "JAVA":
-        return parser_jv.parse(bytes(input_code, "utf-8")).root_node.sexp(), parser_jv.parse(bytes(input_code, "utf-8"))
-    return parser_py.parse(bytes(input_code, "utf-8")).root_node.sexp(), parser_py.parse(bytes(input_code, "utf-8"))
+        return parser_jv.parse(bytes(input_code, "utf-8")).root_node.sexp()
+    return parser_py.parse(bytes(input_code, "utf-8")).root_node.sexp()
 
 
 
+concepts = Concept()
 
 
-concept = Concept()
-concept.save_concepts()
+quanching = "name = [1,2,3,4]\n name.sort()"
 
 
-
-input_code = "list_ = [1,3,4,2]\n list_.sort()"
-tree_one = parser_py.parse(bytes(input_code, "utf-8")).root_node.sexp()
-
-input_code_two = "list_ = [1,3,4,2,2,7,5]\n list_.sort()"
-tree_two = parser_py.parse(bytes(input_code_two, "utf-8")).root_node.sexp()
-
-input_code_three = "list_ = [1,3,4,2,2,7,5]\n list_f = [1,3,4,2,2,7,5]\nlist_ g= [1,3,4,2,2,7,5]\nlist_.sort()"
-tree_three = parser_py.parse(bytes(input_code_three, "utf-8")).root_node.sexp()
-
-print(fuzz.partial_ratio(tree_one, tree_two))
-print(fuzz.partial_ratio(tree_one, tree_three))
+concepts.check_similarity(quanching,PYTHON)
