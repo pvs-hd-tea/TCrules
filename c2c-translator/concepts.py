b46f73b835cdcd1fd60cccf67c72c844321006b3
Vivian Kazakova <the.vivkaz@gmail.com>
2022-08-26 19:03:45 +0300

    remove duplicates

--- c2c-translator/concepts.py
+++ c2c-translator/concepts.py
@@ -4,15 +4,23 @@ from tree_sitter import Language, Parser
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
+
 Language.build_library(
     # Store the library in the `build` directory
     'build/my-languages.so',
 
     # Include one or more languages
+    # Jonas
+    #[
+    #    '/Users/jonas/Documents/GitHub/tree-sitter-cpp',
+    #    '/Users/jonas/Documents/GitHub/tree-sitter-java',
+    #    '/Users/jonas/Documents/GitHub/tree-sitter-python'
+    #]
+    # Vivian
     [
-        '/Users/jonas/Documents/GitHub/tree-sitter-cpp',
-        '/Users/jonas/Documents/GitHub/tree-sitter-java',
-        '/Users/jonas/Documents/GitHub/tree-sitter-python'
+        '/home/vivi/src/tree-sitter-python',
+        '/home/vivi/src/tree-sitter-java',
+        '/home/vivi/src/tree-sitter-cpp'
     ]
 )
 
@@ -51,8 +59,8 @@ class Concept:
         else:
             with open("keywords_concept.txt", encoding="utf8") as file:
                 self.tree_concepts = file.read().split(",")
-                print(
-                    f"Loading {len(self.tree_concepts)} keywords for concepts ... Done ...")
+                print(f"Loading {len(self.tree_concepts)} keywords for concepts ... Done ...")
+
 
     def save_concepts(self):
         """store derived rules in json format"""
@@ -60,6 +68,7 @@ class Concept:
             file.truncate(0)
             file.seek(0)
             json.dump(self.concepts, file, indent=4)
+
 
     def save_keywords(self):
         """store keywords from own json"""
@@ -72,6 +81,7 @@ class Concept:
                     file.write(keyword)
                 else:
                     file.write(keyword + ",")
+
 
     def extend_concept(self, cpp, java, python, key):
         """extend existing concept '<key>' by list of generic expressions, one for each language"""
@@ -88,8 +98,8 @@ class Concept:
                     break  # already present
 
             if not flag:
-                self.concepts[key].append(
-                    [generic_cpp, generic_jv, generic_py])
+                self.concepts[key].append([generic_cpp, generic_jv, generic_py])
+
 
     def add_concept(self, cpp, java, python, key):
         """add new concept to the database"""
@@ -98,8 +108,8 @@ class Concept:
         generic_py = self.create_generic_expression(python, PYTHON.lower())
 
         if generic_cpp and generic_jv and generic_py:
-            self.concepts.update(
-                {key: [[generic_cpp, generic_jv, generic_py]]})
+            self.concepts.update({key: [[generic_cpp, generic_jv, generic_py]]})
+
 
     def user_input(self, keyword=None):
         """Check for well known concept"""
@@ -118,50 +128,34 @@ class Concept:
         else:
             self.add_concept(cpp, java, python, keyword)
 
+
     def check_similarity(self, input_code, input_language):
-
         parse_tree = create_parse_tree(input_code, input_language)
-        if input_language == "PYTHON":
-            lang_id = "py"
-        if input_language == "JAVA":
-            lang_id = "jv"
-        if input_language == "CPP":
-            lang_id = "cpp"
-
-        if input_language == JAVA:
-            lang_matches = []
-            for concept in self.tree_concepts:
-                if lang_id in concept:
-                    tree = parser_jv.parse(
-                        bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
-                    lang_matches.append(tree)
-                    final_concept = process.extractOne(
-                        parse_tree, lang_matches, scorer=fuzz.partial_ratio)
-                    if final_concept[-1] >= 80:
-                        concept_final = concept
-        if input_language == CPP:
-            lang_matches = []
-            for concept in self.tree_concepts:
-                if lang_id in concept:
-                    tree = parser_cpp.parse(
-                        bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
-                    lang_matches.append(tree)
-                    final_concept = process.extractOne(
-                        parse_tree, lang_matches, scorer=fuzz.partial_ratio)
-                    if final_concept[-1] >= 80:
-                        concept_final = concept
+    
         if input_language == PYTHON:
-            lang_matches = []
-            for concept in self.tree_concepts:
-                if lang_id in concept:
-                    tree = parser_py.parse(
-                        bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
-                    lang_matches.append(tree)
-                    final_concept = process.extractOne(
-                        parse_tree, lang_matches, scorer=fuzz.partial_ratio)
-                    if final_concept[-1] >= 80:
-                        concept_final = concept
-        print(concept_final + "has thet highest posssibility of being a correct match. Suggestion for optimized code is in suggestion.txt")
+            lang_id = "py"
+        elif input_language == JAVA:
+            lang_id = "jv"
+        elif input_language == CPP:
+            lang_id = "cpp"
+        else:
+            return
+
+        lang_matches = []
+        for concept in self.tree_concepts:
+            if lang_id in concept:
+                if input_language == JAVA:
+                    tree = parser_jv.parse(bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
+                elif input_language == CPP:
+                    tree = parser_cpp.parse(bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
+                elif input_language == PYTHON:
+                    tree = parser_py.parse(bytes(self.concepts[concept][0][0], "utf-8")).root_node.sexp()
+                lang_matches.append(tree)
+                final_concept = process.extractOne(parse_tree, lang_matches, scorer=fuzz.partial_ratio)
+                if final_concept[-1] >= 80:
+                    concept_final = concept
+
+        print(concept_final + " has the highest posssibility of being a correct match. Suggestion for optimized code is in suggestion.txt")
         with open('suggestion.txt', 'w') as datas:
             concept_string = concept_final[:-3]
             concept_string_py = concept_string + "_py"
@@ -169,19 +163,13 @@ class Concept:
             concept_string_cpp = concept_string + "_cpp"
 
             datas.write("PYTHON:\n")
-            datas.write(str(self.concepts[concept_string_py][0][0]))
-            datas.write("\n")
+            datas.write(str(self.concepts[concept_string_py][0][0])+"\n")
             datas.write("---------------------------------------------\n")
             datas.write("JAVA:\n")
-            datas.write(str(self.concepts[concept_string_jv][0][0]))
-            datas.write("\n")
+            datas.write(str(self.concepts[concept_string_jv][0][0])+"\n")
             datas.write("---------------------------------------------\n")
             datas.write("CPP:\n")
             datas.write(str(self.concepts[concept_string_cpp][0][0]))
-
-
-
-        
 
 
 def create_parse_tree(input_code, input_language):
@@ -196,8 +184,6 @@ def create_parse_tree(input_code, input_language):
 
 concepts = Concept()
 
+quanching = "name = [1,3,5,7,9]\nname.sort()"
 
-quanching = "name = [1,2,3,4]\n name.sort()"
-
-
-concepts.check_similarity(quanching,PYTHON)
+concepts.check_similarity(quanching, PYTHON)
